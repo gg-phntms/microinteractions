1d38452d54e671302aa1163b43ba88e74be2b1d7
Balraj <balraj.johal@phntms.com>
2024-05-07 17:14:47 +0100

    Switch prod url env variable to vercel provided one

--- src/config/vercel.ts
+++ src/config/vercel.ts
@@ -1,6 +1,6 @@
 const DEPLOYED_URL =
-  process.env.NEXT_PUBLIC_URL ?? // A custom env variable representing the final domain URL.
-  process.env.NEXT_PUBLIC_VERCEL_URL; //The domain name of the generated deployment URL.
+  process.env.VERCEL_PROJECT_PRODUCTION_URL ?? // The domain name of the production deployment URL.
+  process.env.NEXT_PUBLIC_VERCEL_URL; // The domain name of the generated deployment URL.
 
 export const FULL_DEPLOYED_URL = DEPLOYED_URL
   ? `https://${DEPLOYED_URL}`
