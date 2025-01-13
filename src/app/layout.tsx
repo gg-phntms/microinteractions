462b6f5d71a630b33d06c1cf7d11977bc504156f
George Gloyens <george.gloyens@phntms.com>
2025-01-13 11:02:48 +0000

    Add metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,8 @@ import RootBody from "@/components/Layout/RootBody";
 import "@/theme/global.scss";
 
 export const metadata: Metadata = {
-  title: "Next Starter",
-  description: "CHANGE ME :)",
+  title: "Micro-interactions",
+  description: "A collection of simple animations to explore and adapt.",
 };
 
 interface Props {
